8c7a9a36cdf5f13a2ede08773ee836f1cd9be1bb
kalizsolt@gmail.com <Igiih2ah1>
2016-01-19 16:20:09 +0100

    Add unit test for Tv Show search REST service.

--- src/main/java/me/twis/controller/TvShowController.java
+++ src/main/java/me/twis/controller/TvShowController.java
@@ -1,14 +1,15 @@
 package me.twis.controller;
 
+import me.twis.util.TheMovieDbUriBuilder;
 import me.twis.entity.Episode;
 import me.twis.entity.TvShowConfiguration;
 import me.twis.entity.TvShowSearchResult;
 import me.twis.entity.Validation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,61 +18,44 @@ import javax.websocket.server.PathParam;
 /**
  * Created by kalizsolt on 10/01/16.
  */
-@Controller
+@RestController
 public class TvShowController {
-    private static final String MOVIE_DB_HOST = "https://api.themoviedb.org/3";
-    private static final String MOVIE_DB_SEARCH_TV = "search/tv";
-    private static final String MOVIE_DB_SEARCH_EPISODE = "tv";
-    public static final String MOVIE_DB_CONFIGURATION = "configuration";
-
-    private static final String PATH_PARAM_QUERY = "query=";
-    private static final String PATH_PARAM_API_KEY = "api_key=";
+    @Value("${themoviedb.url}")
+    private String theMovieDbRootUrl;
 
     @Value("${themoviedb.apikey}")
     private String movieDbApiKey;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/search/tv-show")
-    @ResponseBody
     public TvShowSearchResult searchTvShowByTitle(@PathParam("title") String title) {
-        RestTemplate restTemplate = new RestTemplate();
-        TvShowSearchResult result = restTemplate.getForObject(getSearchTvShowByTitleUrl(title), TvShowSearchResult.class);
+        TvShowSearchResult result = restTemplate.getForObject(TheMovieDbUriBuilder.getSearchTvShowByTitleUrl(title), TvShowSearchResult.class);
         result.updateTvShowWithPosterPathPrefix(getPosterUrl());
 
         return result;
     }
 
-    private String getSearchTvShowByTitleUrl(@PathVariable String title) {
-        return MOVIE_DB_HOST + "/" + MOVIE_DB_SEARCH_TV + "?" +
-                PATH_PARAM_QUERY + title + "&" + PATH_PARAM_API_KEY + movieDbApiKey;
+    @RequestMapping("/dummy")
+    public String getDummy() {
+        return "dummy string";
     }
 
     private String getPosterUrl() {
-        RestTemplate restTemplate = new RestTemplate();
-        TvShowConfiguration configuration = restTemplate.getForObject(getConfigurationUrl(), TvShowConfiguration.class);
+        TvShowConfiguration configuration = restTemplate.getForObject(TheMovieDbUriBuilder.getConfigurationUrl(), TvShowConfiguration.class);
 
         return configuration.getImages().getBaseUrl() + configuration.getImages().getPosterSizes().get(0);
     }
 
-    private String getConfigurationUrl() {
-        return MOVIE_DB_HOST + "/" + MOVIE_DB_CONFIGURATION + "?" + PATH_PARAM_API_KEY + movieDbApiKey;
-    }
-
     @RequestMapping("/validate/tv-show/id/{id}/season/{season}/episode/{episode}")
-    @ResponseBody
     private Validation validateEpisode(@PathVariable int id, @PathVariable int season, @PathVariable int episode) {
-        RestTemplate restTemplate = new RestTemplate();
-
         try {
-            restTemplate.getForObject(getRetrieveEpisodeInformationUrl(id, season, episode), Episode.class);
+            restTemplate.getForObject(TheMovieDbUriBuilder.getRetrieveEpisodeInformationUrl(id, season, episode), Episode.class);
         } catch (HttpClientErrorException ex) {
             return new Validation(false);
         }
 
         return new Validation(true);
-    }
-
-    private String getRetrieveEpisodeInformationUrl(@PathVariable int id, @PathVariable int season, @PathVariable int episode) {
-        return MOVIE_DB_HOST + "/" + MOVIE_DB_SEARCH_EPISODE + "/" + id + "/season/" +
-                season + "/episode/" + episode + "?" + PATH_PARAM_API_KEY + movieDbApiKey;
     }
 }
--- src/test/java/me/twis/controller/TvShowControllerTest.java
+++ src/test/java/me/twis/controller/TvShowControllerTest.java
@@ -1,7 +1,78 @@
 package me.twis.controller;
 
+import me.twis.entity.TvShow;
+import me.twis.entity.TvShowConfiguration;
+import me.twis.entity.TvShowImages;
+import me.twis.entity.TvShowSearchResult;
+import me.twis.util.TheMovieDbUriBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.MockRestServiceServer.createServer;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+
 /**
  * Created by kalizsolt on 10/01/16.
  */
+
 public class TvShowControllerTest {
+    public static final String TV_SHOW_TITLE = "TvShowTitle";
+    public static final int TV_SHOW_ID = 123;
+    public static final String TV_SHOW_IMAGES_BASE_URL = "tvShowImagesBaseUrl";
+    public static final String POSTER_SIZE = "posterSize";
+    public static final String PROP_KEY_MOVIE_DB_API_KEY = "movieDbApiKey";
+    public static final String PROP_VALUE_API_KEY = "apiKey";
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private TvShowController underTest;
+
+    private MockRestServiceServer mockServer;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockServer = createServer(restTemplate);
+        ReflectionTestUtils.setField(underTest, PROP_KEY_MOVIE_DB_API_KEY, PROP_VALUE_API_KEY);
+    }
+
+    @Test
+    public void testDummyWithRestTemplate() throws Exception {
+        TvShowSearchResult mockedResponse = new TvShowSearchResult();
+        TvShow tvShow = new TvShow();
+        tvShow.setId(TV_SHOW_ID);
+        mockedResponse.setResults(Arrays.asList(tvShow));
+        when(restTemplate.getForObject(TheMovieDbUriBuilder.getSearchTvShowByTitleUrl(TV_SHOW_TITLE), TvShowSearchResult.class)).thenReturn(mockedResponse);
+
+        TvShowConfiguration tvShowConfiguration = new TvShowConfiguration();
+        TvShowImages tvShowImages = new TvShowImages();
+        tvShowImages.setBaseUrl(TV_SHOW_IMAGES_BASE_URL);
+        tvShowImages.setPosterSizes(Arrays.asList(POSTER_SIZE));
+        tvShowConfiguration.setImages(tvShowImages);
+        when(restTemplate.getForObject(TheMovieDbUriBuilder.getConfigurationUrl(), TvShowConfiguration.class)).thenReturn(tvShowConfiguration);
+
+        mockServer
+                .expect(requestTo(TheMovieDbUriBuilder.getSearchTvShowByTitleUrl(TV_SHOW_TITLE)))
+                .andExpect(MockRestRequestMatchers.method(HttpMethod.GET))
+                .andRespond(MockRestResponseCreators.withSuccess());
+
+        TvShowSearchResult tvShowSearchResult = underTest.searchTvShowByTitle(TV_SHOW_TITLE);
+
+        Assert.assertEquals(TV_SHOW_ID, tvShowSearchResult.getResults().get(0).getId());
+    }
 }
